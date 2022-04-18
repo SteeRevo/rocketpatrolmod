16495e4ba21e1dd57d876e3caf485fdb84ff9809
heyethan1@gmail.com <heyethan1@gmail.com>
2022-04-18 15:05:03 -0700

    changed menu

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -29,13 +29,26 @@ class Menu extends Phaser.Scene {
             },
             fixedWidth: 0
         }
+
+        let menuConfig2 = {
+          fontFamily: 'Courier',
+          fontSize: '20px',
+          backgroundColor: '#F3B141',
+          color: '#843605',
+          align: 'right',
+          padding: {
+              top: 5,
+              bottom: 5,
+          },
+          fixedWidth: 0
+      }
         
         // show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
         this.add.text(game.config.width/2, game.config.height/2, 'Use ←→ arrows to move & (L) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert', menuConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press ← for Novice or → for Expert, ↑ for Two Player', menuConfig2).setOrigin(0.5);
 
         // define keys
         keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
